8c94d82d2a37c4ad079f620be85b6a3c70299395
Sarah <Sr73xah@gmail.com>
2023-03-29 22:58:10 +0300

    Complete GuideMe.dart frontend

--- eye/lib/GuideMe.dart
+++ eye/lib/GuideMe.dart
@@ -22,29 +22,43 @@ class GuideMe extends StatefulWidget {
 class _GuideMeState extends State<GuideMe> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: appBarHome(
-          title: "أرشدني",
-          context: context,
-          photo: "no",
-          icon: Icons.question_mark_rounded,
-          isBottom: true),
-      body: Center(
-        child: Column(
-          children: [Container()],
+    Size size = MediaQuery.of(context).size;
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        // appBar: appBarHome(
+        //     title: "أرشدني",
+        //     context: context,
+        //     photo: "no",
+        //     icon: Icons.question_mark_rounded,
+        //     isBottom: true),
+        body: Container(
+          margin: EdgeInsets.only(top: 50),
+          child: Center(
+            child: Column(
+              children: [
+                guideMeCard("assets/مركز بلاغات.png", "مركز بلاغات قريبة"),
+                guideMeCard("assets/أرقام تهمك.png", "أرقام تهمك"),
+                guideMeCard("assets/فقدت شخص.png", "فقدت شخص"),
+                guideMeCard("assets/وجدت مفقود.png", "وجدت مفقود"),
+                guideMeCard("assets/أدعية.png", "أدعية لمن فقد شيئًا"),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 
-  GestureDetector guideMeCard() {
+  GestureDetector guideMeCard(String photo, String title) {
     return GestureDetector(
       onTap: () {},
       child: Container(
-        margin: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         padding: const EdgeInsets.all(10),
-        height: 70,
+        height: 100,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5),
@@ -61,11 +75,37 @@ class _GuideMeState extends State<GuideMe> {
           ],
           color: Colors.white,
         ),
+        //------------------------------------------------------
         child: Row(
-            //Icon
-
-            //Text
+          children: [
+            //photo--------------------------
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                margin: const EdgeInsets.only(left: 10),
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(photo), fit: BoxFit.contain),
+                ),
+              ),
             ),
+            SizedBox(
+              width: 10,
+            ),
+            //Text----------------------------
+            Text(
+              title,
+              style: const TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Almarai",
+                  color: primaryDarkGrean,
+                  height: 1.5),
+            ),
+          ],
+        ),
       ),
     );
   }
